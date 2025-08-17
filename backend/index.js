8e3a33ba6eb95f73370990abbc2d9d129517dbdb
Eshaan-14 <eshaanmane954386@gmail.com>
2025-08-17 21:34:37 +0000

    made url changes

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 // Serve frontend's static files
